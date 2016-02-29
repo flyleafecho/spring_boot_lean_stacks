0c129e45b4e1234cdae1211e62b8397008e233d0
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2016-02-29 13:25:48 -0300

    FIrst class: Spring Boot presentation

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot main Application
+ * @author rodrigo
+ *
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(Application.class, args);
+	}
+
+}
