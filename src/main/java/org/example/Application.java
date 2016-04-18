a08b2726bdc8a95bffeed585f6deaa1e175e53a5
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2016-04-18 18:47:20 -0300

    Video finished - Asynchronous processesses

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.guava.GuavaCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableCaching
 @EnableScheduling
+@EnableAsync
 public class Application {
 
 	public static void main(String[] args) throws Exception {
