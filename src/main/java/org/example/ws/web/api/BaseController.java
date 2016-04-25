388142d3abab7b942dd97dde383919322d222cf5
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2016-04-25 15:04:58 -0300

    Video finished: Protecting Application Assets with Spring Security

--- src/main/java/org/example/ws/web/api/BaseController.java
+++ src/main/java/org/example/ws/web/api/BaseController.java
@@ -1,7 +1,12 @@
 package org.example.ws.web.api;
 
-import javax.persistence.NoResultException;
+import java.util.Map;
 
+import javax.persistence.NoResultException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.example.ws.web.DefaultExceptionAttributes;
+import org.example.ws.web.ExceptionAttributes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,20 +16,31 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class BaseController {
 
 	protected Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@ExceptionHandler(NoResultException.class)
-	public ResponseEntity<Exception> handleNoResultException(NoResultException nre) {
+	public ResponseEntity<Map<String, Object>> handleNoResultException(NoResultException nre,
+			HttpServletRequest httpRequest) {
 		logger.error("> handleNoResultException");
 		logger.error("- NoResultException: ", nre);
+
+		ExceptionAttributes exceptionAttributes = new DefaultExceptionAttributes();
+		Map<String, Object> responseBody = exceptionAttributes.getExceptionAttributes(nre, httpRequest,
+				HttpStatus.NOT_FOUND);
+		
 		logger.error("< handleNoResultException");
-		return new ResponseEntity<Exception>(HttpStatus.NOT_FOUND);
+		return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.NOT_FOUND);
 	}
-	
+
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<Exception> handleException(Exception e) {
+	public ResponseEntity<Map<String, Object>> handleException(Exception e, HttpServletRequest httpRequest) {
 		logger.error("> handleException");
 		logger.error("- Exception: ", e);
+
+		ExceptionAttributes exceptionAttributes = new DefaultExceptionAttributes();
+		Map<String, Object> responseBody = exceptionAttributes.getExceptionAttributes(e, httpRequest,
+				HttpStatus.INTERNAL_SERVER_ERROR);
+
 		logger.error("< handleException");
-		return new ResponseEntity<Exception>(HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<Map<String, Object>>(responseBody, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
